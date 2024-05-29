4984c50d0a3df46d1fb38569435b6b82dddc10a1
Kauã <126888652+Kaua-R-d-S@users.noreply.github.com>
2024-05-29 22:48:54 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-perguntas");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-alternativas");
+const caixaPrincipal = document.querySelector(".caixa-resultado");
+const caixaPrincipal = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 2",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 3",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 4",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 5",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  }
+];
