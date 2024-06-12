10bb8e4930cd1a813c07e45f26386e61f0ac9ca1
Kauã <126888652+Kaua-R-d-S@users.noreply.github.com>
2024-06-12 23:48:40 +0000

    12/06/24

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-const caixaPrincipal = document.querySelector(".caixa-perguntas");
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPrincipal = document.querySelector(".caixa-alternativas");
-const caixaPrincipal = document.querySelector(".caixa-resultado");
-const caixaPrincipal = document.querySelector(".texto-resultado");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-  {
-    enunciado: "Pergunta 1",
-    alternativas: [
-      "Resposta 1",
-      "Resposta 2",
-    ]
-  },
+    {
+      enunciado: "assim que você saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele também gera imagens e aúdios super realistas. Qual o seu primeiro pensamento?",
+      alternativas: [
+        "Isso é assustador",
+        "Isso é maravilhoso"
+      ]
+    },
 
-  {
-    enunciado: "Pergunta 2",
-    alternativas: [
-      "Resposta 1",
-      "Resposta 2",
-    ]
-  },
+    {
+      enunciado: "Com a descoberta desta tecnologia, chamda inteligencia artifical (IA), uma prfessora de tecnoloogia de uma escola decidiu fazer muma sequência de aulas sobre essa ferramenta. No fim de uma aula ela pede que você escreva uma trabalho sobre o uso da IA em sala de aula. Qual atitude você toma?",
+      alternativas: [
+        "utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique em uma linguagem que facilite o entendimnto.",
+        "escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos proprios sobre o tema."
+      ]
+    },
 
-  {
-    enunciado: "Pergunta 3",
-    alternativas: [
-      "Resposta 1",
-      "Resposta 2",
-    ]
-  },
+    {
+      enunciado: "Pergunta 3",
+      alternativas: [
+        "Resposta 1",
+        "Resposta 2"
+      ]
+    },
 
-  {
-    enunciado: "Pergunta 4",
-    alternativas: [
-      "Resposta 1",
-      "Resposta 2",
-    ]
-  },
+    {
+      enunciado: "Pergunta 4",
+      alternativas: [
+        "Resposta 1",
+        "Resposta 2"
+      ]
+    },
 
-  {
-    enunciado: "Pergunta 5",
-    alternativas: [
-      "Resposta 1",
-      "Resposta 2",
-    ]
-  }
+    {
+      enunciado: "Pergunta 5",
+      alternativas: [
+        "Resposta 1",
+        "Resposta 2"
+      ]
+    }
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+
+mostraPergunta();
